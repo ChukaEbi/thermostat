10e43fa493647436005b7aaae7daa5811bb44855
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-11 11:21:36 +0000

    Can decrease temp but not past min temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -12,4 +12,17 @@ describe("Thermostat", function() {
     thermostat.increaseTemp();
     expect(thermostat.temperature).toEqual(21);
   });
+
+  it("should decrease the temperature by 1 degree", function(){
+    thermostat.decreaseTemp();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it("minimum temperature should be 10 degrees", function(){
+    for(i=0;i<10;i++){
+      thermostat.decreaseTemp();
+    }
+    message = 'Cannot decrease temperature any further';
+    expect(function(){thermostat.decreaseTemp();}).toThrow(new Error(message));
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,20 @@
 function Thermostat() {
     var DEFAULT_TEMP = 20;
+    this.MIN_TEMP = 10;
     this.temperature = DEFAULT_TEMP;
 }
 
 Thermostat.prototype.temperature = function() {
   return this.temperature;
-}
+};
 
 Thermostat.prototype.increaseTemp = function() {
   this.temperature++;
-}
+};
+
+Thermostat.prototype.decreaseTemp = function(){
+  if(this.temperature <= this.MIN_TEMP){
+    throw new Error('Cannot decrease temperature any further');
+  }
+  this.temperature--;
+};
