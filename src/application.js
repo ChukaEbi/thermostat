8962b2390024688a88e27272f3b8444b248245d2
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-12 15:09:54 +0000

    Refactored

--- src/application.js
+++ src/application.js
@@ -3,28 +3,26 @@ $(document).ready(function(){
   $('#display').prepend(thermostat.temperature);
   $('#increaseTemp').on('click',function(){
     thermostat.increaseTemp();
-    $('#display').replaceWith("<span id='display'>" + thermostat.temperature + "</span>");
-    console.log(thermostat.temperature);
-  });
-  $('#decreaseTemp').on('click',function(){
-    thermostat.decreaseTemp();
-    $('#display').replaceWith("<span id='display'>" + thermostat.temperature + "</span>");
-    console.log(thermostat.temperature);
-  });
-  $('#resetTemp').on('click',function(){
-    thermostat.resetTemp();
-    $('#display').replaceWith("<span id='display'>" + thermostat.temperature + "</span>");
-    console.log(thermostat.temperature);
-  });
-  $('#psmOn').on('click',function(){
-    thermostat.powerSavingModeOn();
-    $('#psm').replaceWith("<span id='psm'>" + 'on' + "</span>");
-    console.log(thermostat.temperature);
-  });
-  $('#psmOff').on('click',function(){
-    thermostat.powerSavingModeOff();
-    $('#psm').replaceWith("<span id='psm'>" + 'off' + "</span>");
-    console.log(thermostat.temperature);
+    $('#display').text(thermostat.temperature);
   });
 
+  $('#decreaseTemp').on('click',function(){
+    thermostat.decreaseTemp();
+    $('#display').text(thermostat.temperature);
+  });
+
+  $('#resetTemp').on('click',function(){
+    thermostat.resetTemp();
+    $('#display').text(thermostat.temperature);
+  });
+
+  $('#psmOn').on('click',function(){
+    thermostat.powerSavingModeOn();
+    $('#psm').text('on');
+  });
+
+  $('#psmOff').on('click',function(){
+    thermostat.powerSavingModeOff();
+    $('#psm').text('off');
+  });
 });
